a1b6a381382ac0b7a2c37e925ba11cd362610ee2
Lamers <gustav@henrikssons.org>
2024-08-31 18:28:14 +0200

    asd

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCHmJ2mMVOnhmJMO3eY1ouwmzHoH9R2T-0',
-  authDomain: 'holidays-415709.firebaseapp.com',
-  projectId: 'holidays-415709',
+  apiKey: 'AIzaSyDtFkjSXKRYXwqYUV4aFYsK24kf0JW8BFc',
+  authDomain: 'listig-428113.firebaseapp.com',
+  projectId: 'listig-428113',
 };
 
 const app = initializeApp(firebaseConfig);
